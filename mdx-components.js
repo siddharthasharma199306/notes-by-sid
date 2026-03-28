6c4d8c5bb082764db32ed009b93bcbb4030971c4
siddharthasharma199306 <siddhartha1993.06@gmail.com>
2026-03-29 00:44:40 +0530

    Some more notes

--- mdx-components.js
+++ mdx-components.js
@@ -1,4 +1,5 @@
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs"; // nextra-theme-blog or your custom theme
+import PatternCallout from "./app/_components/pattern-callout";
 
 // Get the default MDX components
 const themeComponents = getThemeComponents();
@@ -7,6 +8,7 @@ const themeComponents = getThemeComponents();
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
+    PatternCallout,
     ...components,
   };
 }
